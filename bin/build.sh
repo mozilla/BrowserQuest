56df1d52de68ccf78ea832d336fb4e3dcd27058d
Dave Eddy <dave@daveeddy.com>
2012-03-28 13:23:45 -0700

    Cleaning up build.sh
    * Quoting all variables to avoid possible word splitting

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script to generate an optimized client build of BrowserQuest
 
@@ -6,15 +6,14 @@ TOPLEVELDIR="`dirname $0`/.."
 BUILDDIR="$TOPLEVELDIR/client-build"
 PROJECTDIR="$TOPLEVELDIR/client/js"
 
-
 echo "Deleting previous build directory"
-rm -rf $BUILDDIR
+rm -rf "$BUILDDIR"
 
 echo "Building client with RequireJS"
-node $TOPLEVELDIR/bin/r.js -o $PROJECTDIR/build.js
+node "$TOPLEVELDIR/bin/r.js" -o "$PROJECTDIR/build.js"
 
 echo "Removing unnecessary js files from the build directory"
-find $BUILDDIR/js -type f \
+find "$BUILDDIR/js" -type f \
   -not \( -name "game.js" \
   -o -name "home.js" \
   -o -name "log.js" \
@@ -28,12 +27,12 @@ find $BUILDDIR/js -type f \
   -delete
 
 echo "Removing sprites directory"
-rm -rf $BUILDDIR/sprites
+rm -rf "$BUILDDIR/sprites"
 
 echo "Removing config directory"
-rm -rf $BUILDDIR/config
+rm -rf "$BUILDDIR/config"
 
 echo "Moving build.txt to current dir"
-mv $BUILDDIR/build.txt $TOPLEVELDIR
+mv "$BUILDDIR/build.txt" "$TOPLEVELDIR"
 
 echo "Build complete"
