e5fed84b7f8c275b2fd48a343b41cb8a01aaa73b
mkrecny <mkrecny@gmail.com>
2012-03-28 14:06:17 -0400

    static servers /client not /client-build

--- static/app.js
+++ static/app.js
@@ -10,9 +10,11 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-var static_dir = __dirname + '/../client-build';
-var index = fs.readFileSync(__dirname+'/../client-build/index.html', 'utf8');
-//var gametypes = fs.readFileSync(__dirname+'/../shared/js/gametypes.js', 'utf8');
+//var static_dir = __dirname + '/../client-build';
+var static_dir = __dirname + '/../client';
+//var index = fs.readFileSync(__dirname+'/../client-build/index.html', 'utf8');
+var index = fs.readFileSync(__dirname+'/../client/index.html', 'utf8');
+var gametypes = fs.readFileSync(__dirname+'/../shared/js/gametypes.js', 'utf8');
 
 app.configure(function(){
   app.use(express.bodyParser());
@@ -34,8 +36,8 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.end(index);
 });
-/*app.get('/shared/js/gametypes.js', function(req, res){
+app.get('/shared/js/gametypes.js', function(req, res){
   res.end(gametypes);
-});*/
+});
 app.listen(8001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
