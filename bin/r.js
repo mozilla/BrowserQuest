abd406c85266ce91f240a2ff0e218a61610b77ab
Scott Noel-Hemming <frogstarr78@gmail.com>
2012-04-08 19:58:46 -0700

    Updating code to remove deprecation warning when using path.exists and path.existsSync.

--- bin/r.js
+++ bin/r.js
@@ -88,7 +88,7 @@ var requirejs, require, define;
         };
 
         exists = function (fileName) {
-            return path.existsSync(fileName);
+            return fs.existsSync(fileName);
         };
 
 
@@ -2218,7 +2218,7 @@ var requirejs, require, define;
         context.loaded[moduleName] = false;
         context.scriptCount += 1;
 
-        if (path.existsSync(url)) {
+        if (fs.existsSync(url)) {
             contents = fs.readFileSync(url, 'utf8');
 
             contents = req.makeNodeWrapper(contents);
