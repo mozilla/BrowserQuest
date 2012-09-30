72c58c1f04455c2a559471084ac53739752ed53e
Justin Clift <jclift@redhat.com>
2012-09-30 23:32:29 +1000

    Adjusted npc.js for JSHint.

--- server/js/npc.js
+++ server/js/npc.js
@@ -1,7 +1,15 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true,
+    unused:true, curly:true, browser:true, node:true, indent:4, maxerr:50, globalstrict:true,
+    camelcase: true, quotmark: single, trailing: true*/
+
+'use strict';
+
 var Entity = require('./entity');
 
-module.exports = Npc = Entity.extend({
-    init: function(id, kind, x, y) {
-        this._super(id, "npc", kind, x, y);
+var Npc = Entity.extend({
+    init: function (id, kind, x, y) {
+        this._super(id, 'npc', kind, x, y);
     }
 });
+
+module.exports = Npc;
