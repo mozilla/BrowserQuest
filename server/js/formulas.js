9f860b8c9a5326b74782dc323f92dbe98e0bb7fd
Justin Clift <jclift@redhat.com>
2012-10-03 16:25:53 +1000

    Updated formulas.js for JSHint.

--- server/js/formulas.js
+++ server/js/formulas.js
@@ -1,26 +1,29 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:false, undef:true,
+    unused:true, curly:true, browser:true, node:true, maxerr:50, camelcase: true, quotmark: single,
+    trailing: true*/
 
-var Utils = require("./utils");
+var Utils = require('./utils');
 
 var Formulas = {};
 
-Formulas.dmg = function(weaponLevel, armorLevel) {
-    var dealt = weaponLevel * Utils.randomInt(5, 10),
-        absorbed = armorLevel * Utils.randomInt(1, 3),
-        dmg =  dealt - absorbed;
+Formulas.dmg = function (weaponLevel, armorLevel) {
+    var dealt = weaponLevel * Utils.randomInt(5, 10);
+    var absorbed = armorLevel * Utils.randomInt(1, 3);
+    var dmg =  dealt - absorbed;
 
     //console.log("abs: "+absorbed+"   dealt: "+ dealt+"   dmg: "+ (dealt - absorbed));
-    if(dmg <= 0) {
+    if (dmg <= 0) {
         return Utils.randomInt(0, 3);
     } else {
         return dmg;
     }
 };
 
-Formulas.hp = function(armorLevel) {
+Formulas.hp = function (armorLevel) {
     var hp = 80 + ((armorLevel - 1) * 30);
     return hp;
 };
 
-if(!(typeof exports === 'undefined')) {
+if (typeof exports !== 'undefined') {
     module.exports = Formulas;
 }
