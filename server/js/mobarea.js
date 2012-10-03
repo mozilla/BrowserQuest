2aa81398076e352d00322aeb6cc1dd0f7a92efe7
Justin Clift <jclift@redhat.com>
2012-10-03 21:18:14 +1000

    Updated mobarea.js for JSHint.

--- server/js/mobarea.js
+++ server/js/mobarea.js
@@ -1,10 +1,14 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:false, undef:true,
+    unused:true, curly:true, browser:true, node:true, maxerr:50, camelcase: true, quotmark: single,
+    trailing: true*/
 
 var _ = require('underscore');
 var Area = require('./area');
-var Types = require("../../shared/js/gametypes");
+var Types = require('../../shared/js/gametypes');
+var Utils = require('./utils');
 
-module.exports = MobArea = Area.extend({
-    init: function(id, nb, kind, x, y, width, height, world) {
+var MobArea = Area.extend({
+    init: function (id, nb, kind, x, y, width, height, world) {
         this._super(id, x, y, width, height, world);
         this.nb = nb;
         this.kind = kind;
@@ -14,15 +18,15 @@ module.exports = MobArea = Area.extend({
         //this.initRoaming();
     },
 
-    spawnMobs: function() {
-        for(var i = 0; i < this.nb; i += 1) {
+    spawnMobs: function () {
+        for (var i = 0; i < this.nb; i += 1) {
             this.addToArea(this._createMobInsideArea());
         }
     },
 
-    _createMobInsideArea: function() {
-        var Mob = require('./mob');
+    _createMobInsideArea: function () {
         var k = Types.getKindFromString(this.kind);
+        var Mob = require('./mob');
         var pos = this._getRandomPositionInsideArea();
         var mob = new Mob('1' + this.id + '' + k + '' + this.entities.length, k, pos.x, pos.y);
 
@@ -31,12 +35,12 @@ module.exports = MobArea = Area.extend({
         return mob;
     },
 
-    respawnMob: function(mob, delay) {
+    respawnMob: function (mob, delay) {
         var self = this;
 
         this.removeFromArea(mob);
 
-        setTimeout(function() {
+        setTimeout(function () {
             var pos = self._getRandomPositionInsideArea();
 
             mob.x = pos.x;
@@ -47,16 +51,16 @@ module.exports = MobArea = Area.extend({
         }, delay);
     },
 
-    initRoaming: function(mob) {
+    initRoaming: function (mob) {
         var self = this;
 
-        setInterval(function() {
-            _.each(self.entities, function(mob) {
-                var canRoam = (Utils.random(20) === 1),
-                    pos;
+        setInterval(function () {
+            _.each(self.entities, function (mob) {
+                var canRoam = (Utils.random(20) === 1);
+                var pos;
 
-                if(canRoam) {
-                    if(!mob.hasTarget() && !mob.isDead) {
+                if (canRoam) {
+                    if (!mob.hasTarget() && !mob.isDead) {
                         pos = self._getRandomPositionInsideArea();
                         mob.move(pos.x, pos.y);
                     }
@@ -65,9 +69,11 @@ module.exports = MobArea = Area.extend({
         }, 500);
     },
 
-    createReward: function() {
+    createReward: function () {
         var pos = this._getRandomPositionInsideArea();
 
         return { x: pos.x, y: pos.y, kind: Types.Entities.CHEST };
     }
 });
+
+module.exports = MobArea;
